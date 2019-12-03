0e2da54c5a3a978860b1e6dfb5953e65ab653ad2
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2019-12-03 18:40:15 -0300

    Projeto criado

---
+++ src/main/java/com/thiagonishimura/controle_celular/ControleCelularApplication.java
@@ -0,0 +1,13 @@
+package com.thiagonishimura.controle_celular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleCelularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleCelularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiagonishimura/controle_celular/ControleCelularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagonishimura.controle_celular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleCelularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
