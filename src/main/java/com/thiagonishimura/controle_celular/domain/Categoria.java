760fe73d945187e0fb4e57e2e99d2d2f6fa0d44e
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2019-12-05 18:40:12 -0300

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/com/thiagonishimura/controle_celular/domain/Categoria.java
+++ src/main/java/com/thiagonishimura/controle_celular/domain/Categoria.java
@@ -1,7 +1,15 @@
 package com.thiagonishimura.controle_celular.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
