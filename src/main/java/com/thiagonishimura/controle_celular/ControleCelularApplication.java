8802d90eeafed7476add144356bbc3219cb96157
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2019-12-09 17:36:40 -0300

    Criando operação de instanciação

--- src/main/java/com/thiagonishimura/controle_celular/ControleCelularApplication.java
+++ src/main/java/com/thiagonishimura/controle_celular/ControleCelularApplication.java
@@ -1,13 +1,31 @@
 package com.thiagonishimura.controle_celular;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ControleCelularApplication {
+import com.thiagonishimura.controle_celular.domain.Categoria;
+import com.thiagonishimura.controle_celular.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ControleCelularApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ControleCelularApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Tela");
+		Categoria cat2 = new Categoria(null, "Conector");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));		
+	}
 }
