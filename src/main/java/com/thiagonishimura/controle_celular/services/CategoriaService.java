25b774926639b4eebe375f3407a213f978b59a1e
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2019-12-19 15:14:08 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/thiagonishimura/controle_celular/services/CategoriaService.java
+++ src/main/java/com/thiagonishimura/controle_celular/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.thiagonishimura.controle_celular.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.thiagonishimura.controle_celular.domain.Categoria;
 import com.thiagonishimura.controle_celular.repositories.CategoriaRepository;
+import com.thiagonishimura.controle_celular.services.exceptions.DataIntegrityException;
 import com.thiagonishimura.controle_celular.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
