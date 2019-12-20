7a1da30b3aa1209fc469e323c9081c6438078219
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2019-12-20 13:14:39 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/thiagonishimura/controle_celular/services/CategoriaService.java
+++ src/main/java/com/thiagonishimura/controle_celular/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.thiagonishimura.controle_celular.domain.Categoria;
+import com.thiagonishimura.controle_celular.dto.CategoriaDTO;
 import com.thiagonishimura.controle_celular.repositories.CategoriaRepository;
 import com.thiagonishimura.controle_celular.services.exceptions.DataIntegrityException;
 import com.thiagonishimura.controle_celular.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
