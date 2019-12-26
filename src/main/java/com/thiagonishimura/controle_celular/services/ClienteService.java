cf7d0b0d62d7d7605fcbd2b415d014c2c2b5245d
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2019-12-26 14:31:37 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/thiagonishimura/controle_celular/services/ClienteService.java
+++ src/main/java/com/thiagonishimura/controle_celular/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.thiagonishimura.controle_celular.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.thiagonishimura.controle_celular.domain.Cidade;
 import com.thiagonishimura.controle_celular.domain.Cliente;
+import com.thiagonishimura.controle_celular.domain.Endereco;
+import com.thiagonishimura.controle_celular.domain.enums.TipoCliente;
 import com.thiagonishimura.controle_celular.dto.ClienteDTO;
+import com.thiagonishimura.controle_celular.dto.ClienteNewDTO;
 import com.thiagonishimura.controle_celular.repositories.ClienteRepository;
+import com.thiagonishimura.controle_celular.repositories.EnderecoRepository;
 import com.thiagonishimura.controle_celular.services.exceptions.DataIntegrityException;
 import com.thiagonishimura.controle_celular.services.exceptions.ObjectNotFoundException;
 
@@ -21,7 +28,18 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+	
 	public Cliente find (Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -60,5 +78,20 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 }
