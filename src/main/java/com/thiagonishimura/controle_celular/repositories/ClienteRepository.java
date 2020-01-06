a886fcf4cad5f523871073d73d247ba4b33a4a29
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2020-01-06 17:23:58 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/thiagonishimura/controle_celular/repositories/ClienteRepository.java
+++ src/main/java/com/thiagonishimura/controle_celular/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package com.thiagonishimura.controle_celular.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.thiagonishimura.controle_celular.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 
 }
